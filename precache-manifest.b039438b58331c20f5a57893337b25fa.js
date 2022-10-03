b352ba5b995b0a26ccde61df428c975182891b31
OlehS <olehjs16@gmail.com>
2022-10-03 08:45:34 -0500

    Updates

---
+++ precache-manifest.b039438b58331c20f5a57893337b25fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc9da6e7a90f19aed77b",
+    "url": "/tesla-clone/static/css/main.3563b248.chunk.css"
+  },
+  {
+    "revision": "bc9da6e7a90f19aed77b",
+    "url": "/tesla-clone/static/js/main.bc9da6e7.chunk.js"
+  },
+  {
+    "revision": "3da4e6a8535dd6d8027b",
+    "url": "/tesla-clone/static/js/1.3da4e6a8.chunk.js"
+  },
+  {
+    "revision": "f910b5b4c8adce963a60",
+    "url": "/tesla-clone/static/js/2.f910b5b4.chunk.js"
+  },
+  {
+    "revision": "0135c64e357ee0bb7480",
+    "url": "/tesla-clone/static/js/runtime~main.0135c64e.js"
+  },
+  {
+    "revision": "515129e04c64a84a5ba0db6a5b2947a2",
+    "url": "/tesla-clone/index.html"
+  }
+];
